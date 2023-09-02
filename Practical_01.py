0a55335183dc80502994bfa83108776c8182219f
Yash Dhage <dhageyash15@gmail.com>
2023-09-02 12:35:36 +0530

    Sentence Correction, Tokenization, Stop Word, Lemmitization

---
+++ Practical_01.py
@@ -0,0 +1,63 @@
+# 01 Sentence Detection
+
+import spacy
+import nlp
+
+about_text = (
+    "Gus Proto is a Python developer currently"
+    " working for a London-based Fintech"
+    " company. He is interested in learning"
+    " Natural Language Processing."
+)
+about_doc = nlp(about_text)
+sentences = list(about_doc.sents)
+len(sentences)
+
+for sentence in sentences:
+    print(f"{sentence[:5]}...")
+
+
+# 02 Token
+
+import spacy
+nlp = spacy.load("en_core_web_sm")
+about_text = (
+    "Gus Proto is a Python developer currently"
+    " working for a London-based Fintech"
+    " company. He is interested in learning"
+    " Natural Language Processing."
+)
+about_doc = nlp(about_text)
+
+for token in about_doc:
+    print (token, token.idx)
+
+
+# 03 Stop Word
+
+import spacy
+custom_about_text = (
+    "Gus Proto is a Python developer currently"
+    " working for a London-based Fintech"
+    " company. He is interested in learning"
+    " Natural Language Processing."
+)
+nlp = spacy.load("en_core_web_sm")
+about_doc = nlp(custom_about_text)
+print([token for token in about_doc if not token.is_stop])
+
+
+# 04 Lemmatization
+
+import spacy
+nlp = spacy.load("en_core_web_sm")
+conference_help_text = (
+    "Gus is helping organize a developer"
+    " conference on Applications of Natural Language"
+    " Processing. He keeps organizing local Python meetups"
+    " and several internal talks at his workplace."
+)
+conference_help_doc = nlp(conference_help_text)
+for token in conference_help_doc:
+    if str(token) != str(token.lemma_):
+        print(f"{str(token):>20} : {str(token.lemma_)}")
--- first.py
+++ first.py
@@ -1,3 +1,2 @@
 print("HII")
-
-Print("Yash");
+print("Yash Dhage")
